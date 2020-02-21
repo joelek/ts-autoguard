a9edd34f39be8056c5b6113f3c2cf8809854c15d
Joel Ek <contact@joelek.se>
2020-02-21 14:28:24 +0100

    Make standalone an option.

--- source/autoguard-lib/index.ts
+++ source/autoguard-lib/index.ts
@@ -1,13 +1,16 @@
+import * as guards from "./guards";
 import * as language from "./language";
 import * as serialization from "./serialization";
 
 function transform(string: string): string {
 	return language.Schema.parse(string).generateModule({
-		eol: "\r\n"
+		eol: "\r\n",
+		standalone: true
 	});
 }
 
 export {
+	guards,
 	language,
 	serialization,
 	transform
