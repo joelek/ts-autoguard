086dbdd6637ce9352ce19e3ef7d416db11c34d58
Joel Ek <contact@joelek.se>
2021-06-09 11:39:50 +0200

    Add better structure to code generation.

--- build/autoguard-idl/schema.js
+++ build/autoguard-idl/schema.js
@@ -119,6 +119,113 @@ function getResponseType(route) {
     });
     return response;
 }
+function generateClientRoute(route, options) {
+    let lines = new Array();
+    let tag = makeRouteTag(route);
+    lines.push(`async (request) => {`);
+    lines.push(`\tlet guard = shared.Autoguard.Requests["${tag}"];`);
+    lines.push(`\tguard.as(request, "request");`);
+    lines.push(`\tlet method = "${route.method.method}";`);
+    lines.push(`\tlet components = new Array<string>();`);
+    for (let component of route.path.components) {
+        if (is.absent(component.type)) {
+            lines.push(`\tcomponents.push(decodeURIComponent("${encodeURIComponent(component.name)}"));`);
+        }
+        else {
+            lines.push(`\tcomponents.push(String(request.options["${component.name}"]));`);
+        }
+    }
+    let exclude = new Array();
+    for (let component of route.path.components) {
+        if (is.present(component.type)) {
+            exclude.push(`"${component.name}"`);
+        }
+    }
+    lines.push(`\tlet parameters = autoguard.api.extractKeyValuePairs(request.options ?? {}, [${exclude.join(",")}]);`);
+    lines.push(`\tlet headers = autoguard.api.extractKeyValuePairs(request.headers ?? {});`);
+    if (route.request.payload === types.Binary.INSTANCE) {
+        lines.push(`\tlet payload = request.payload;`);
+    }
+    else {
+        lines.push(`\tlet payload = autoguard.api.serializePayload(request.payload);`);
+    }
+    lines.push(`\tlet requestHandler = options?.requestHandler ?? autoguard.api.xhr;`);
+    lines.push(`\tlet raw = await requestHandler({ method, components, parameters, headers, payload }, options?.urlPrefix);`);
+    lines.push(`\t{`);
+    lines.push(`\t\tlet status = raw.status;`);
+    lines.push(`\t\tlet headers = autoguard.api.combineKeyValuePairs(raw.headers);`);
+    for (let header of route.response.headers.headers) {
+        lines.push(`\t\theaders["${header.name}"] = autoguard.api.${makeParser(header.type)}(raw.headers, "${header.name}");`);
+    }
+    if (route.response.payload === types.Binary.INSTANCE) {
+        lines.push(`\t\tlet payload = raw.payload;`);
+    }
+    else {
+        lines.push(`\t\tlet payload = await autoguard.api.deserializePayload(raw.payload);`);
+    }
+    lines.push(`\t\tlet guard = shared.Autoguard.Responses["${tag}"];`);
+    lines.push(`\t\tlet response = guard.as({ status, headers, payload }, "response");`);
+    lines.push(`\t\treturn new autoguard.api.ServerResponse(response);`);
+    lines.push(`\t}`);
+    lines.push(`}`);
+    return lines.join(options.eol);
+}
+function generateServerRoute(route, options) {
+    let lines = new Array();
+    let tag = makeRouteTag(route);
+    lines.push(`(raw, auxillary) => {`);
+    lines.push(`\tlet method = "${route.method.method}";`);
+    lines.push(`\tlet components = new Array<[string, string]>();`);
+    for (let [index, component] of route.path.components.entries()) {
+        if (is.present(component.type)) {
+            lines.push(`\tcomponents.push(["${component.name}", raw.components[${index}]]);`);
+        }
+        else {
+            lines.push(`\tcomponents.push(["", decodeURIComponent("${encodeURIComponent(component.name)}")]);`);
+        }
+    }
+    lines.push(`\treturn {`);
+    lines.push(`\t\tacceptsComponents: () => autoguard.api.acceptsComponents(raw.components, components),`);
+    lines.push(`\t\tacceptsMethod: () => autoguard.api.acceptsMethod(raw.method, method),`);
+    lines.push(`\t\tvalidateRequest: async () => {`);
+    lines.push(`\t\t\tlet options = autoguard.api.combineKeyValuePairs(raw.parameters);`);
+    for (let component of route.path.components) {
+        if (is.present(component.type)) {
+            lines.push(`\t\t\toptions["${component.name}"] = autoguard.api.${makeParser(component.type)}(components, "${component.name}");`);
+        }
+    }
+    for (let parameter of route.parameters.parameters) {
+        lines.push(`\t\t\toptions["${parameter.name}"] = autoguard.api.${makeParser(parameter.type)}(raw.parameters, "${parameter.name}");`);
+    }
+    lines.push(`\t\t\tlet headers = autoguard.api.combineKeyValuePairs(raw.headers);`);
+    for (let header of route.request.headers.headers) {
+        lines.push(`\t\t\theaders["${header.name}"] = autoguard.api.${makeParser(header.type)}(raw.headers, "${header.name}");`);
+    }
+    if (route.request.payload === types.Binary.INSTANCE) {
+        lines.push(`\t\t\tlet payload = raw.payload;`);
+    }
+    else {
+        lines.push(`\t\t\tlet payload = await autoguard.api.deserializePayload(raw.payload);`);
+    }
+    lines.push(`\t\t\tlet guard = shared.Autoguard.Requests["${tag}"];`);
+    lines.push(`\t\t\tlet request = guard.as({ options, headers, payload }, "request");`);
+    lines.push(`\t\t\treturn {`);
+    lines.push(`\t\t\t\thandleRequest: async () => {`);
+    lines.push(`\t\t\t\t\tlet response = await routes["${tag}"](new autoguard.api.ClientRequest(request, auxillary));`);
+    lines.push(`\t\t\t\t\treturn {`);
+    lines.push(`\t\t\t\t\t\tvalidateResponse: async () => {`);
+    lines.push(`\t\t\t\t\t\t\tlet guard = shared.Autoguard.Responses["${tag}"];`);
+    lines.push(`\t\t\t\t\t\t\tguard.as(response, "response");`);
+    lines.push(`\t\t\t\t\t\t\treturn response;`);
+    lines.push(`\t\t\t\t\t\t}`);
+    lines.push(`\t\t\t\t\t};`);
+    lines.push(`\t\t\t\t}`);
+    lines.push(`\t\t\t};`);
+    lines.push(`\t\t}`);
+    lines.push(`\t};`);
+    lines.push(`}`);
+    return lines.join(options.eol);
+}
 class Schema {
     constructor(guards, routes) {
         this.guards = guards;
@@ -182,52 +289,7 @@ class Schema {
         lines.push(`}>): autoguard.api.Client<shared.Autoguard.Requests, shared.Autoguard.Responses> => ({`);
         for (let route of this.routes) {
             let tag = makeRouteTag(route);
-            lines.push(`\t"${tag}": async (request) => {`);
-            lines.push(`\t\tlet guard = shared.Autoguard.Requests["${tag}"];`);
-            lines.push(`\t\tguard.as(request, "request");`);
-            lines.push(`\t\tlet method = "${route.method.method}";`);
-            lines.push(`\t\tlet components = new Array<string>();`);
-            for (let component of route.path.components) {
-                if (is.absent(component.type)) {
-                    lines.push(`\t\tcomponents.push(decodeURIComponent("${encodeURIComponent(component.name)}"));`);
-                }
-                else {
-                    lines.push(`\t\tcomponents.push(String(request.options["${component.name}"]));`);
-                }
-            }
-            let exclude = new Array();
-            for (let component of route.path.components) {
-                if (is.present(component.type)) {
-                    exclude.push(`"${component.name}"`);
-                }
-            }
-            lines.push(`\t\tlet parameters = autoguard.api.extractKeyValuePairs(request.options ?? {}, [${exclude.join(",")}]);`);
-            lines.push(`\t\tlet headers = autoguard.api.extractKeyValuePairs(request.headers ?? {});`);
-            if (route.request.payload === types.Binary.INSTANCE) {
-                lines.push(`\t\tlet payload = request.payload;`);
-            }
-            else {
-                lines.push(`\t\tlet payload = autoguard.api.serializePayload(request.payload);`);
-            }
-            lines.push(`\t\tlet requestHandler = options?.requestHandler ?? autoguard.api.xhr;`);
-            lines.push(`\t\tlet raw = await requestHandler({ method, components, parameters, headers, payload }, options?.urlPrefix);`);
-            lines.push(`\t\t{`);
-            lines.push(`\t\t\tlet status = raw.status;`);
-            lines.push(`\t\t\tlet headers = autoguard.api.combineKeyValuePairs(raw.headers);`);
-            for (let header of route.response.headers.headers) {
-                lines.push(`\t\t\theaders["${header.name}"] = autoguard.api.${makeParser(header.type)}(raw.headers, "${header.name}");`);
-            }
-            if (route.response.payload === types.Binary.INSTANCE) {
-                lines.push(`\t\t\tlet payload = raw.payload;`);
-            }
-            else {
-                lines.push(`\t\t\tlet payload = await autoguard.api.deserializePayload(raw.payload);`);
-            }
-            lines.push(`\t\t\tlet guard = shared.Autoguard.Responses["${tag}"];`);
-            lines.push(`\t\t\tlet response = guard.as({ status, headers, payload }, "response");`);
-            lines.push(`\t\t\treturn new autoguard.api.ServerResponse(response);`);
-            lines.push(`\t\t}`);
-            lines.push(`\t},`);
+            lines.push(`\t"${tag}": ${generateClientRoute(route, Object.assign(Object.assign({}, options), { eol: options.eol + "\t" }))},`);
         }
         lines.push(`});`);
         lines.push(``);
@@ -243,58 +305,7 @@ class Schema {
         lines.push(`export const makeServer = (routes: autoguard.api.Server<shared.Autoguard.Requests, shared.Autoguard.Responses>, options?: Partial<{ urlPrefix: string }>): autoguard.api.RequestListener => {`);
         lines.push(`\tlet endpoints = new Array<autoguard.api.Endpoint>();`);
         for (let route of this.routes) {
-            let tag = makeRouteTag(route);
-            lines.push(`\tendpoints.push((raw, auxillary) => {`);
-            lines.push(`\t\tlet method = "${route.method.method}";`);
-            lines.push(`\t\tlet components = new Array<[string, string]>();`);
-            for (let [index, component] of route.path.components.entries()) {
-                if (is.present(component.type)) {
-                    lines.push(`\t\tcomponents.push(["${component.name}", raw.components[${index}]]);`);
-                }
-                else {
-                    lines.push(`\t\tcomponents.push(["", decodeURIComponent("${encodeURIComponent(component.name)}")]);`);
-                }
-            }
-            lines.push(`\t\treturn {`);
-            lines.push(`\t\t\tacceptsComponents: () => autoguard.api.acceptsComponents(raw.components, components),`);
-            lines.push(`\t\t\tacceptsMethod: () => autoguard.api.acceptsMethod(raw.method, method),`);
-            lines.push(`\t\t\tvalidateRequest: async () => {`);
-            lines.push(`\t\t\t\tlet options = autoguard.api.combineKeyValuePairs(raw.parameters);`);
-            for (let component of route.path.components) {
-                if (is.present(component.type)) {
-                    lines.push(`\t\t\t\toptions["${component.name}"] = autoguard.api.${makeParser(component.type)}(components, "${component.name}");`);
-                }
-            }
-            for (let parameter of route.parameters.parameters) {
-                lines.push(`\t\t\t\toptions["${parameter.name}"] = autoguard.api.${makeParser(parameter.type)}(raw.parameters, "${parameter.name}");`);
-            }
-            lines.push(`\t\t\t\tlet headers = autoguard.api.combineKeyValuePairs(raw.headers);`);
-            for (let header of route.request.headers.headers) {
-                lines.push(`\t\t\t\theaders["${header.name}"] = autoguard.api.${makeParser(header.type)}(raw.headers, "${header.name}");`);
-            }
-            if (route.request.payload === types.Binary.INSTANCE) {
-                lines.push(`\t\t\t\tlet payload = raw.payload;`);
-            }
-            else {
-                lines.push(`\t\t\t\tlet payload = await autoguard.api.deserializePayload(raw.payload);`);
-            }
-            lines.push(`\t\t\t\tlet guard = shared.Autoguard.Requests["${tag}"];`);
-            lines.push(`\t\t\t\tlet request = guard.as({ options, headers, payload }, "request");`);
-            lines.push(`\t\t\t\treturn {`);
-            lines.push(`\t\t\t\t\thandleRequest: async () => {`);
-            lines.push(`\t\t\t\t\t\tlet response = await routes["${tag}"](new autoguard.api.ClientRequest(request, auxillary));`);
-            lines.push(`\t\t\t\t\t\treturn {`);
-            lines.push(`\t\t\t\t\t\t\tvalidateResponse: async () => {`);
-            lines.push(`\t\t\t\t\t\t\t\tlet guard = shared.Autoguard.Responses["${tag}"];`);
-            lines.push(`\t\t\t\t\t\t\t\tguard.as(response, "response");`);
-            lines.push(`\t\t\t\t\t\t\t\treturn response;`);
-            lines.push(`\t\t\t\t\t\t\t}`);
-            lines.push(`\t\t\t\t\t\t};`);
-            lines.push(`\t\t\t\t\t}`);
-            lines.push(`\t\t\t\t};`);
-            lines.push(`\t\t\t}`);
-            lines.push(`\t\t};`);
-            lines.push(`\t});`);
+            lines.push(`\tendpoints.push(${generateServerRoute(route, Object.assign(Object.assign({}, options), { eol: options.eol + "\t" }))});`);
         }
         lines.push(`\treturn (request, response) => autoguard.api.route(endpoints, request, response, options?.urlPrefix);`);
         lines.push(`};`);
