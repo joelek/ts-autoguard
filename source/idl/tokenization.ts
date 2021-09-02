94f89732aac8f18908b5159d52d5b3b2a32d7f6d
Joel Ek <contact@joelek.se>
2021-09-02 09:57:30 +0200

    Require tokens to be percent-encoded in path components.

--- source/idl/tokenization.ts
+++ source/idl/tokenization.ts
@@ -139,7 +139,7 @@ export class Tokenizer {
 			"IDENTIFIER": /^([a-zA-Z][a-zA-Z0-9_]*)/su,
 			"NUMBER_LITERAL": /^(([1-9][0-9]+)|([0-9]))/su,
 			"STRING_LITERAL": /^(["][^"]*["])/su,
-			"PATH_COMPONENT": /^(([a-zA-Z0-9_.~-]|[%][0-9a-fA-F]{2})+)/su,
+			"PATH_COMPONENT": /^(([a-zA-Z0-9_]|[%][0-9a-fA-F]{2})+)/su,
 			"COMMENT": /^([#][^\r\n]*)/su
 		};
 		let tokens = new Array<Token>();
