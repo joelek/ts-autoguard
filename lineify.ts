2d4d78b0dff5ada1d0105fd756eeac9949d1832a
Joel Ek <contact@joelek.se>
2023-05-26 16:46:46 +0200

    Replace all occurrences of backslash in lineify.

--- lineify.ts
+++ lineify.ts
@@ -1,3 +1,3 @@
 import * as fs from "fs";
 
-fs.readFileSync(process.argv[2], "utf8").split(/\r?\n/).map((line) => console.log(`lines.push(\`${line.replace("\\", "\\\\")}\`);`));
+fs.readFileSync(process.argv[2], "utf8").split(/\r?\n/).map((line) => console.log(`lines.push(\`${line.replace(/\\/g, "\\\\")}\`);`));
