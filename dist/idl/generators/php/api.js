acfd0edf380bc422b0fbcac1ebb7662da6b66696
Joel Ek <contact@joelek.se>
2023-09-10 11:09:56 +0200

    Generate dist artifacts.

--- dist/idl/generators/php/api.js
+++ dist/idl/generators/php/api.js
@@ -189,7 +189,7 @@ class PHPAPIGenerator extends generator_1.Generator {
         }
         for (let [index, { name, quantifier, type }] of route.path.components.entries()) {
             if (type != null) {
-                lines.push(`		$request->options->{"${name}"} = $this->matchers[${index}]->get_value();`);
+                lines.push(`		$this->matchers[${index}]->read_value($request->options, "${name}");`);
             }
         }
         for (let [index, { name, quantifier, type }] of route.parameters.parameters.entries()) {
@@ -597,11 +597,13 @@ class PHPAPIGenerator extends generator_1.Generator {
         lines.push(`		return $this->accept_component($component, false);`);
         lines.push(`	}`);
         lines.push(``);
-        lines.push(`	function get_value(): mixed {`);
+        lines.push(`	function read_value(object $target, string $key): void {`);
         lines.push(`		if ($this->max_occurences === 1) {`);
-        lines.push(`			return $this->values[0];`);
+        lines.push(`			if (count($this->values) > 0) {`);
+        lines.push(`				$target->$key = $this->values[0];`);
+        lines.push(`			}`);
         lines.push(`		} else {`);
-        lines.push(`			return $this->values;`);
+        lines.push(`			$target->$key = $this->values;`);
         lines.push(`		}`);
         lines.push(`	}`);
         lines.push(``);
